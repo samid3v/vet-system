36d068da2638ef1323aa8f0adc49f0993e411283
samd3v <swanyoike154@gmail.com>
2023-11-08 01:51:29 +0300

    routing update

--- client/src/Dashboard/App.jsx
+++ client/src/Dashboard/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Dash from './pages/Dashboard'
+import Patients from './pages/Patients'
+import AllPatients from './pages/Patients/All'
 
 function App() {
 
   return (
-    <>
-      index page
-    </>
+    <BrowserRouter basename='/dashboard'>
+      <Routes>
+        <Route index element={<Dash/>}/>
+        <Route path='/patients' element={<Patients/>}>
+          <Route index element={<AllPatients/>}/>
+        </Route>
+      </Routes>    
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Dashboard/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dash = () => {
+  return (
+    <div>
+      das
+    </div>
+  )
+}
+
+export default Dash
