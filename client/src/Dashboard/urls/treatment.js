9e127b1cf5c4d952abae41cff025e8f3d4cf3c1f
samd3v <swanyoike154@gmail.com>
2023-12-29 17:50:21 +0300

    treatment axios urls

---
+++ client/src/Dashboard/urls/treatment.js
@@ -0,0 +1,16 @@
+const treatmentUrl = {
+     get_all:{
+          url:`/treatments/all`
+     },
+     delete_transaction:{
+          url:`/transactions/delete-transaction`
+     },
+     add_transaction:{
+          url:`/treatments/add-treatment`
+     },
+     get_patient:{
+          url:`/transactions/get-transaction`
+     },
+}
+
+export default treatmentUrl
