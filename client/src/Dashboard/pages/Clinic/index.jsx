6222d2e0245692fc5e26c016355d1ab723911f05
samd3v <swanyoike154@gmail.com>
2024-01-31 17:18:34 +0300

    clinic search filter update

--- client/src/Dashboard/pages/Clinic/index.jsx
+++ client/src/Dashboard/pages/Clinic/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import ClinicProvider from './Provider'
+import FilterModal from '../../components/FilterModal'
 
 const Clinic = () => {
   
   return (
     <ClinicProvider >
         <Outlet/>
+        <FilterModal/>
+
     </ClinicProvider>
   )
 }
