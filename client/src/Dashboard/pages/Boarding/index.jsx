f6f811d50a1962fe40cda5b23f90917ec60a4968
samd3v <swanyoike154@gmail.com>
2024-01-31 10:10:39 +0300

    modal ui search filter update

--- client/src/Dashboard/pages/Boarding/index.jsx
+++ client/src/Dashboard/pages/Boarding/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import BoardingProvider from './Provider'
+import FilterModal from '../../components/FilterModal'
 
 const Boarding = () => {
   
   return (
     <BoardingProvider >
         <Outlet/>
+        <FilterModal/>
     </BoardingProvider>
   )
 }
